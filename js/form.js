3d9cb0daf042765d1661b12f07c47d846fb24831
Koblizka <flood.nya@gmail.com>
2021-06-21 21:03:39 +0300

    Правит функции состояния страницы

--- js/form.js
+++ js/form.js
@@ -1,21 +1,41 @@
 
 const adForm = document.querySelector('.ad-form');
-const adFormInputs = adForm.querySelectorAll('fieldset');
-
-const disablePage = (isTrue) => {
-  if (isTrue) {
-    adForm.classList.add('ad-form--disabled');
-
-    adFormInputs.forEach((item) => {
-      item.setAttribute('disabled', '');
-    });
-  } else {
-    adForm.classList.remove('ad-form--disabled');
-
-    adFormInputs.forEach((item) => {
-      item.removeAttribute('disabled');
-    });
-  }
+const adFormFieldsets = adForm.querySelectorAll('fieldset');
+const mapFiltersInputs = document.querySelector('.map__filters').querySelectorAll('[class^=map__]');
+const state = {
+  active: true,
+  deactive: false,
 };
 
-disablePage(true);
+const setFormState = (formItems, isActivate) => {
+  if (isActivate) {
+    formItems.forEach((item) => {
+      item.removeAttribute('disabled');
+    });
+
+    return true;
+  }
+
+  formItems.forEach((item) => {
+    item.setAttribute('disabled', '');
+  });
+
+  return false;
+};
+
+const setPageState = (isActive) => {
+  if (isActive) {
+    adForm.classList.remove('ad-form--disabled');
+    setFormState(adFormFieldsets, state.active);
+    setFormState(mapFiltersInputs, state.active);
+    return true;
+  }
+
+  adForm.classList.add('ad-form--disabled');
+  setFormState(adFormFieldsets, state.deactive);
+  setFormState(mapFiltersInputs, state.deactive);
+
+  return false;
+};
+
+setPageState(state.deactive);
